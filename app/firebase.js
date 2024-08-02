760739a0c9ba81c3fe1e7540d1d9b94cca16ebd3
developersbm <sbastidamarin@student.cccd.edu>
2024-08-03 01:06:12 +0200

    .

--- app/firebase.js
+++ app/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 
